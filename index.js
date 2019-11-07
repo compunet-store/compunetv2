1ff0078ceab4593336676f6c0c7b12c36f26c2ee
Cristiam Díaz <c.diaz@udla.edu.co>
2019-11-07 17:36:35 -0500

    change view engine hbs instead ejs

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 let express = require('express')
 let app = express()
-let ejs = require("ejs")
- 
-app.set('view engine','ejs')
+let exphbs = require('express-handlebars')
+let path = require('path')
 
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+  })
+)
+app.set('view engine', '.hbs')
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('products')
 })
 
 app.listen(3000)
